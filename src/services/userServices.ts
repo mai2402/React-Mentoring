9e86e0bc387aaa145fbaf754e94ebe4714ed0eb2
mai-fareed <maifareed@apessmartsolutions.com>
2025-06-09 23:23:18 +0300

    migration from vanilla css to sass

---
+++ src/services/userServices.ts
@@ -0,0 +1,41 @@
+import { UserProfile } from "../interfaces/user/user";
+import { supabase } from "../supabase/client";
+
+
+
+export async function getUserProfile() {
+    
+const { data: { user }, error: userError } = await supabase.auth.getUser();
+
+    if (  userError|| !user) {
+        throw new Error("User not found");
+    }
+
+    const {data, error} = await supabase.from("users").select("*").eq("id", user.id).single();
+    
+  if (error) throw new Error(error.message);
+  return data;
+
+}
+
+
+export async function updateUserProfile(updates: Partial<UserProfile>) {
+
+  const { data: { user }, error: userError } = await supabase.auth.getUser();
+
+  if (userError || !user) {
+    throw new Error("User not found");
+
+    
+  }
+  console.log(user?.id)
+
+  const { data, error } = await supabase
+    .from("users")
+    .update(updates)
+    .eq("id", user.id);
+
+  if (error) throw new Error(error.message);
+  return data;
+
+}
