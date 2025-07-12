db12d0732ab44c19e03fc1ea8e43c14d10a2d2fe
Muhammed Yousrii <muhammed.yuosry@gmail.com>
2025-07-13 02:45:05 +0300

    [Dashboard][Sessions](Enhance): Formate date against locale with custom options

--- src/modules/dashboard/config/sessions/sessionsTable.ts
+++ src/modules/dashboard/config/sessions/sessionsTable.ts
@@ -1,11 +1,20 @@
 import { Session } from "../../../sessions/interfaces/session";
 import { HourLabelsEnum } from "./timeLabelsConfig";
 
+// This file defines the columns for the session table in the dashboard.
+const options: Intl.DateTimeFormatOptions = {
+  dateStyle: "medium"
+}
 
 export  const sessionTableColumns = [
     { key: "title", label: "Title" },
-    { key: "date", label: "Date" },
+    // Date Column
+    { 
+      key: "date", 
+      label: "Date",
+      render: (s: Session) => new Date(s.date).toLocaleDateString('en-US', options)},
     { key: "summary", label: "Summary" },
+    // Duration Column
     {
       key: "duration",
       label: "Duration",
